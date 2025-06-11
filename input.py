927b01342b3d259b461c1b056117b863d7437c08
GiovannaKiarha <giovannakiarha@outlook.com>
2025-06-11 17:18:13 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,17 @@
+idade = float(input('Digite a sua idade:'))
+
+nome = str(input('Digite o seu nome:'))
+
+print(f'O meu nome é {nome} e eu tenho {idade} anos')
+
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+
+divisao = num1/num2
+multiplicacao = num1*num2
+soma = num1+num2
+subtracao = num1-num2
+exp = num1**num2
+
+print(divisao, multiplicacao, soma, subtracao, exp) # mostra apenas o resultado, sem texto, sem nada
+
